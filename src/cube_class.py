b681b56541009b1e826e9a9b649b56f57c152580
sophielueth <sophie.lueth@me.com>
2023-01-15 15:02:51 +0100

    Added ColorMap class, stump of CubeClass Cube Class will probably extended when continuing with coding

---
+++ src/cube_class.py
@@ -0,0 +1,29 @@
+
+from geometry_msgs.msg import StampedPose
+
+import rospy
+
+from color_map_class import Color, ColorMap
+
+
+class Cube:
+	"""
+	This class represents one detected cube.
+	pos: np.array with shape (3, ): position in relation to the camera frame
+	rot: float: angle in rad, between 0 and pi/2
+	color: string
+
+	"""
+	def __init__(self, pose_topic, top_color: Color = Color.NONE):
+		self._stacked = False
+		self.pose = None
+		self._pose_topic = pose_topic
+		self._colormap = ColorMap(top_color=top_color)
+
+		# continuously update the pose:
+		rospy.Subscriber(self._pose_topic, StampedPose, self.update_pose)
+
+	def update_pose(self, pose: StampedPose):
+		self.pose = pose
+
+	# TODO: interface to rotate the ClorMap... Not sure yet whether to connect it automatically to the pose or do it extra
