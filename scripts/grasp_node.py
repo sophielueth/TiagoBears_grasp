4faf188655b01708de51dd7c59fc0269b7ec48bc
Daljeet Nandha <sophie.lueth@me.com>
2023-01-25 17:12:46 +0000

    restructuring and added opening and closing gripper

--- scripts/grasp_node.py
+++ scripts/grasp_node.py
@@ -5,12 +5,14 @@ from TiagoBears_grasp.grasp_class import Grasp
 from TiagoBears_grasp.cube_class import Cube
 
 if __name__ == '__main__':
+    rospy.init_node('grasp', anonymous=True)
+
     try:
         grasp = Grasp()
         cube_13 = Cube(13)
 
         grasp.pick(cube_13)
         print('pick ended')
-    except rospy.ROSInterruptException:
-        print('an exception has occured')
-
+    except rospy.ROSInterruptException as e:
+        print('an exception has occured:')
+        print(e)
--- src/TiagoBears_grasp/grasp_class.py
+++ src/TiagoBears_grasp/grasp_class.py
@@ -7,19 +7,26 @@ import numpy as np
 import rospy
 import moveit_commander
 import moveit_msgs.msg
-from geometry_msgs.msg import Pose, PoseStamped
+from geometry_msgs.msg import Pose, Point, Quaternion
 from tf import transformations
+
+from std_msgs.msg import Header
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal, JointTolerance
+from actionlib import SimpleActionClient
+
+
 
 from cube_class import Cube
 
-# MAIN FRAME IS base_footprint
+# MAIN/Basis FRAME is base_footprint
 # later TODO s: 
 # - both arms to move and "work" simulaneously
 # - add some params in the init function to a param .yaml and read out from the parameter server
 # - add a smart wait pose
 
-# list of (current)Z TODO s:
-# - add opening and closing of gripper via gripper_left_controller and gripper_right_controller (Action Client)
+# list of (current) TODO s:
+# - check opening and closing values for the endeffector
 # - check endeffector frame of groups, otherwise convert them, add orientation of approach angle 
 # - add error handling
 
@@ -31,39 +38,29 @@ class Grasp:
 		self._cube_length = 0.045 # in m
 		self._height_over_place = 0.005 # in m 
 		
-		# self._wait_pose_left = Pose() # later TODO see above
+		# self._wait_pose_left = Pose() # later TODO add wait pose above table
 		# self._wait_pose_right = Pose()
 
-		self._look_at_pose_left = Pose() 
-		(self._look_at_pose_left.position.x, self._look_at_pose_left.position.y, self._look_at_pose_left.position.z) = (0.626, 0.107, 0.882) # supposed to be sent to gripper_left_grasping_frame
-		(self._look_at_pose_left.orientation.x, self._look_at_pose_left.orientation.y, self._look_at_pose_left.orientation.z, self._look_at_pose_left.orientation.w) = (0.774, -0.497, 0.356, -0.165)
-		self._look_at_pose_right = Pose()
-		(self._look_at_pose_right.position.x, self._look_at_pose_right.position.y, self._look_at_pose_right.position.z) = (0.608, -0.130, 0.882) # supposed to be sent to gripper_right_grasping_frame
-		(self._look_at_pose_right.orientation.x, self._look_at_pose_right.orientation.y, self._look_at_pose_right.orientation.z, self._look_at_pose_right.orientation.w) = (0.773, 0.494, 0.364, 0.162)
+		self._look_at_pose_left = Pose(Point(0.626, 0.107, 0.882), Quaternion(0.774, -0.497, 0.356, -0.165)) # supposed to be sent to gripper_left_grasping_frame
+		self._look_at_pose_right = Pose(Point(0.608, -0.130, 0.882), Quaternion(0.773, 0.494, 0.364, 0.162)) # supposed to be sent to gripper_right_grasping_frame
 
-		# initialize move it for both arms
+		## initialize move it for both arms
 		moveit_commander.roscpp_initialize(sys.argv)
 
-		## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
-		## kinematic model and the robot's current joint states
+		## Instantiate a `RobotCommander`_ object. Provides information such as the robot's kinematic model and the robot's current joint states
 		self.robot = moveit_commander.RobotCommander(robot_description="robot_description")
 
-		## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
-		## for getting, setting, and updating the robot's internal understanding of the
-		## surrounding world:
+		## Instantiate a `PlanningSceneInterface`_ object; it's a remote interface for getting, setting, and updating the robot's internal understanding of the surrounding world:
 		self.scene = moveit_commander.PlanningSceneInterface()
 
-		## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-		## to a planning group (group of joints).
-		## This interface can be used to plan and execute motions:
+		## Instantiate a `MoveGroupCommander`_ object; it's interface to a planning group (group of joints), used to plan and execute motions:
 		group_name_left = "arm_left"  
 		group_name_right = "arm_right"
 		self.move_group_left = moveit_commander.MoveGroupCommander(group_name_left)
 		self.move_group_right = moveit_commander.MoveGroupCommander(group_name_right)
 
-		## Create a `DisplayTrajectory`_ ROS publisher which is used to display
-		## trajectories in Rviz:
-		# very later TODO: check whether I can have one topic to publish both trajectories
+		## Create a `DisplayTrajectory`_ ROS publisher which is used to display trajectories in Rviz:
+		# very much later TODO: check whether I can have one topic to publish both trajectories
 		self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
 		                                               moveit_msgs.msg.DisplayTrajectory,
 		                                               queue_size=20)
@@ -76,64 +73,127 @@ class Grasp:
 		# move to wait position (for now using the watch position as wait position)
 		self.move_both_to_watch_position()
 
+		# init action clients to move grippers
+		self._gripper_left_client = SimpleActionClient(ns='/gripper_left_controller/follow_joint_trajectory', ActionSpec=FollowJointTrajectory)
+		self._gripper_right_client = SimpleActionClient(ns='/gripper_right_controller/follow_joint_trajectory', ActionSpec=FollowJointTrajectory)
+		self._gripper_left_client.wait_for_server()
+		self._gripper_right_client.wait_for_server()
+
+		# TODO: add correct values
+		self._gripper_closed = [JointTrajectoryPoint(positions=[0.022, 0.022], velocities=np.zeros((2,)), time_from_start=rospy.Duration.from_sec(2))]
+		self._gripper_opened = [JointTrajectoryPoint(positions=[0.04, 0.04], velocities=np.zeros((2,)), time_from_start=rospy.Duration.from_sec(2))]
+		self._gripper_left_joint_names = ['gripper_left_left_finger_joint', 'gripper_left_right_figher_joint']
+		self._gripper_right_joint_names = ['gripper_right_left_finger_joint', 'gripper_rightt_right_figher_joint']
+
+		self.open_gripper()
+
 		# Stuff that might be useful
-		# rospy.init_node('grasp', anonymous=True) is no node
 		# eef_link = move_group.get_end_effector_link()
-		# group_names = robot.get_group_names()
 		# robot.get_current_state()
 
-	def pick(self, cube: Cube):
+	def pick(self, cube: Cube) -> bool:
 		# decide on arm to use
-		use_left_arm = True if cube.pose.y > 0 else False
-		move_group = self.move_group_left if use_left_arm else self.move_group_right
+		use_left = True if cube.pose.y > 0 else False
+		move_group = self.move_group_left if use_left else self.move_group_right
 
 		# create pre-pick (10 cm above pick posistion) & pick position
-		pre_pick_poses = self.get_pre_pickplace_poses(cube.pose)
+		pick_poses = self._get_pre_pickplace_poses(cube.pose)
 		
-		# pick
+		self._execute_pick(move_group, pick_poses)
+		
+		# retract via the pre-pick pose to the watch position
+		self._retract(use_left, pick_poses[0])
+
+		return use_left
+
+	def place(self, use_left:bool, place_pose:Pose,):
+		# select arm to use
+		move_group = self.move_group_left if use_left else self.move_group_right
+
+		# create pre-place (10 cm above place posistion) & place position
+		place_poses = self._get_pre_pickplace_poses(place_pose)
+
+		self._execute_place(use_left, place_poses)
+
+		# create post place position (same as pre-place position) and retract
+		self._retract(use_left, place_poses[0])
+
+	def move_left_to_watch_position(self):
+		plan, _ = self.move_group_left.compute_cartesian_path([self._look_at_pose_left], 0.01, 0.0)
+		self.move_group_left._execute_plan(plan, wait=True)
+
+	def move_right_to_watch_position(self):
+		plan, _ = self.move_group_right.compute_cartesian_path([self._look_at_pose_right], 0.01, 0.0)
+		self.move_group_right._execute_plan(plan, wait=True)
+
+	def move_both_to_watch_position(self):
+		# later TODO: make simultaneuos
+		self.move_left_to_watch_position()
+		self.move_right_to_watch_position()
+
+	def close_gripper(self, use_left:bool):
+		gripper_client, joint_names = self._gripper_left_client, self._gripper_left_joint_names if use_left else self._gripper_right_client, self._gripper_right_joint_names
+		
+		# create goal for the action client for the gripper
+		goal = FollowJointTrajectoryGoal()
+
+		header = Header()
+		header.stamp = rospy.Time.now()
+		goal.trajectory = JointTrajectory(header, joint_names, self._gripper_closed)
+		
+		goal.path_tolerance = JointTolerance() # choose the default values
+		goal.goal_tolerance = goal.path_tolerance
+		goal.goal_time_tolerance = rospy.Duration.from_sec(1.0)
+		
+		gripper_client.send_goal_and_wait(goal=goal, Duration=rospy.Duration.from_sec(5.0))
+
+	def open_gripper(self, use_left:bool):
+		gripper_client, joint_names = self._gripper_left_client, self._gripper_left_joint_names if use_left else self._gripper_right_client, self._gripper_right_joint_names
+		
+		# create goal for the action client for the gripper
+		goal = FollowJointTrajectoryGoal()
+
+		header = Header()
+		header.stamp = rospy.Time.now()
+		goal.trajectory = JointTrajectory(header, joint_names, self._gripper_opened)
+		
+		goal.path_tolerance = JointTolerance() # choose the default values
+		goal.goal_tolerance = goal.path_tolerance
+		goal.goal_time_tolerance = rospy.Duration.from_sec(1.0)
+		
+		gripper_client.send_goal_and_wait(goal=goal, Duration=rospy.Duration.from_sec(5.0))
+
+	def _execute_pick(self, use_left:bool, pick_poses:list):
+		move_group = self.move_group_left if use_left else self.move_group_right
+
 		plan, _ = move_group.compute_cartesian_path(
-			pre_pick_poses,  # waypoints to follow
+			pick_poses,  # waypoints to follow
 			0.01,  # eef_step
 			0.0)  # jump_threshold
 		move_group.execute_plan(plan, wait=True) # later TODO: Somehow enable that the other arm can be started while the first one is in movement
 		
-		# close gripper
-		# TODO
-		
-		# create post grasp position (same as pre-grasp position) and retract
-		plan, _ = move_group.compute_cartesian_path(
-			[pre_pick_poses[0]], 0.01, 0.0)  
-		move_group.execute_plan(plan, wait=True)
+		self.close_gripper()
 
-		# move to watch position
-		self.move_left_to_watch_position() if use_left_arm else self.move_right_to_watch_position()
+	def _execute_place(self, use_left:bool, place_poses:list):
+		move_group = self.move_group_left if use_left else self.move_group_right
 
-		return use_left_arm
-
-	def place(self, place_pose:Pose, use_left_arm:bool):
-		# select arm to use
-		move_group = self.move_group_left if use_left_arm else self.move_group_right
-
-		# create pre-place (10 cm above place posistion) & place position
-		pre_place_poses = self.get_pre_pickplace_poses(place_pose)
-
-		# place
-		plan, _ = move_group.compute_cartesian_path(pre_place_poses, 0.01, 0.0)
+		plan, _ = move_group.compute_cartesian_path(place_poses, 0.01, 0.0)
 	
 		move_group.execute_plan(plan, wait=True)
 		
-		# open gripper
-		# TODO
+		self.open_gripper()
 
-		# create post place position (same as pre-place position) and retract
+	def _retract(self, use_left:bool, retract_pose:Pose):
+		move_group = self.move_group_left if use_left else self.move_group_right
+
 		plan, _ = move_group.compute_cartesian_path(
-			[pre_place_poses[0]], 0.01, 0.0)  
+			[retract_pose], 0.01, 0.0)  
 		move_group.execute_plan(plan, wait=True)
 
 		# move to watch position
-		self.move_left_to_watch_position() if use_left_arm else self.move_right_to_watch_position()
+		self.move_left_to_watch_position() if use_left else self.move_right_to_watch_position()
 
-	def get_pre_pickplace_poses(self, cube_pose:Pose):
+	def _get_pre_pickplace_poses(self, cube_pose:Pose):
 		target_pose = copy.deepcopy(cube_pose)
 		# TODO: use self._approach_angle to calculate it
 		R = np.array([[1,  0,  0, 0],
@@ -146,16 +206,3 @@ class Grasp:
 		pre_target_pose.z += 0.1 # have the end-effector approach from 10 cm above the cube
 
 		return [pre_target_pose, target_pose]
-
-	def move_left_to_watch_position(self):
-		plan, _ = self.move_group_left.compute_cartesian_path([self._look_at_pose_left], 0.01, 0.0)
-		self.move_group_left.execute_plan(plan, wait=True)
-
-	def move_right_to_watch_position(self):
-		plan, _ = self.move_group_right.compute_cartesian_path([self._look_at_pose_right], 0.01, 0.0)
-		self.move_group_right.execute_plan(plan, wait=True)
-
-	def move_both_to_watch_position(self):
-		# later TODO: make simultaneuos
-		self.move_left_to_watch_position()
-		self.move_right_to_watch_position()
