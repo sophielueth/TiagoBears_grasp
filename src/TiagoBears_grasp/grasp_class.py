178508e3c12dcb6e36b0da255fa711e92829fded
sophielueth <sophie.lueth@me.com>
2023-02-13 00:28:53 +0000

    added pick approach position horizontally & rewrote the Grasp object for just 1 arm - untested!

--- src/TiagoBears_grasp/grasp_class.py
+++ src/TiagoBears_grasp/grasp_class.py
@@ -15,8 +15,6 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal, JointTolerance
 from actionlib import SimpleActionClient
 
-
-
 from cube_class import Cube
 
 # MAIN/Basis FRAME is base_footprint
@@ -32,7 +30,7 @@ from cube_class import Cube
 
 
 class Grasp:
-	def __init__(self):
+	def __init__(self, is_left, traj_pub):
 		# later TODO add to param server and read out
 		self._cube_length = 0.045 # in m
 		self._height_over_place = 0.005 # in m 
@@ -44,162 +42,89 @@ class Grasp:
 		self._start_pose = [0.50, 0.00, 2.00, 1.35, -1.57, 0.70, 0.70] # in joint space
 		self._look_at_pose = [0.50, 0.30, 1.25, 1.35, -1.57, 0.70, 0.70]# in joint space
 
-		## initialize move it for both arms
-		moveit_commander.roscpp_initialize(sys.argv)
-
-		## Instantiate a `RobotCommander`_ object. Provides information such as the robot's kinematic model and the robot's current joint states
-		self.robot = moveit_commander.RobotCommander(robot_description="robot_description")
-
-		## Instantiate a `PlanningSceneInterface`_ object; it's a remote interface for getting, setting, and updating the robot's internal understanding of the surrounding world:
-		self.scene = moveit_commander.PlanningSceneInterface()
-
 		## Instantiate a `MoveGroupCommander`_ object; it's interface to a planning group (group of joints), used to plan and execute motions:
-		group_name_left = "arm_left"  
-		group_name_right = "arm_right"
-		self.move_group_left = moveit_commander.MoveGroupCommander(group_name_left)
-		self.move_group_right = moveit_commander.MoveGroupCommander(group_name_right)
-		self.move_group_left.set_planning_time(1.0)
-		self.move_group_right.set_planning_time(1.0)
-		self.move_group_left.set_num_planning_attempts(5)
-		self.move_group_right.set_num_planning_attempts(5)
+		group_name = 'arm_left' if is_left else 'arm_right'
 
-		## Create a `DisplayTrajectory`_ ROS publisher which is used to display trajectories in Rviz:
-		self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-		                                               moveit_msgs.msg.DisplayTrajectory,
-		                                               queue_size=20)
+		self.move_group = moveit_commander.MoveGroupCommander(group_name)
+		self.move_group.set_planning_time(1.0)
+		self.move_group.set_num_planning_attempts(5)
 
-		# init to move torso to joint position 0.25
-		torso_pub = rospy.Publisher('/torso_controller/command', JointTrajectory)
+		self.display_trajectory_publisher = traj_pub
 
-		# Set end_effector_link frame to gripper_left_grasping_frame
-		self.move_group_left.set_end_effector_link('gripper_left_grasping_frame')
-		self.move_group_right.set_end_effector_link('gripper_right_grasping_frame')
+		# Set end_effector_link frame to gripper_xx_grasping_frame
+		ee_link = 'gripper_left_grasping_frame' if is_left else 'gripper_right_grasping_frame'
+		self.move_group.set_end_effector_link(ee_link)
 
 		# move to wait position (for now using the watch position as wait position)
-		self.move_both_to_start_position()
+		self.move_to_start_position()
 
 		# init action clients to move grippers
-		self._gripper_left_client = SimpleActionClient(ns='/gripper_left_controller/follow_joint_trajectory', ActionSpec=FollowJointTrajectoryAction)
-		self._gripper_right_client = SimpleActionClient(ns='/gripper_right_controller/follow_joint_trajectory', ActionSpec=FollowJointTrajectoryAction)
-		self._gripper_left_client.wait_for_server()
-		self._gripper_right_client.wait_for_server()
+		ns = '/gripper_left_controller/follow_joint_trajectory' if is_left else '/gripper_right_controller/follow_joint_trajectory'
+		self._gripper_client = SimpleActionClient(ns=ns, ActionSpec=FollowJointTrajectoryAction)
+		self._gripper_client.wait_for_server()
 
 		# TODO: check closed values
-		self._gripper_closed = [JointTrajectoryPoint(positions=[0.8], time_from_start=rospy.Duration.from_sec(2))]
+		self._gripper_closed = [JointTrajectoryPoint(positions=[0.4], time_from_start=rospy.Duration.from_sec(2))]
 		self._gripper_opened = [JointTrajectoryPoint(positions=[0.0], time_from_start=rospy.Duration.from_sec(2))]
-		self._gripper_left_joint_names = ['gripper_left_finger_joint']
-		self._gripper_right_joint_names = ['gripper_right_finger_joint']
+		self._gripper_joint_names = ['gripper_left_finger_joint'] if is_left else ['gripper_right_finger_joint']
 
-		self.open_gripper(use_left=True)
-		self.open_gripper(use_left=False)
-
-		# actually move torso to joint position 0.25 after publisher has been set up
-		msg = JointTrajectory(joint_names=['torso_lift_joint'], 
-							  points=[JointTrajectoryPoint(positions=[0.25], time_from_start=rospy.Duration.from_sec(1))])
-		torso_pub.publish(msg)
+		self.set_gripper(self._gripper_opened)
 
 		self._rotate_0 = np.array([0, 0, 0, 1])
 		self._rotate_y_45_deg = np.array([0, 0.382, 0,  0.923])
-		self._rotate_z_90_degs = [self._rotate_0, 
-								  np.array([0, 0, 0.707002, 0.7072115]), 
-								  np.array([0, 0, 0.707002, -0.7072115]),
-								   np.array([0, 0, 1, 0.0002963])]
+		self._rotate_z_90_degs = [self._rotate_0, # for the gripper frame to approach from the front
+								 np.array([0, 0, 0.707002, -0.7072115]), # left					
+								 np.array([0, 0, 1, 0.0002963]), # back
+								 np.array([0, 0, 0.707002, 0.7072115])] if is_left else [self._rotate_0, # right for left and front for right 
+																						 np.array([0, 0, 0.707002, 0.7072115]), # right
+												                                         np.array([0, 0, 1, 0.0002963]), # back
+																						 np.array([0, 0, 0.707002, -0.7072115])] # left
 
-		# add table as collision object
-		p = Pose(Point(x=0.5, y=0, z=0), Quaternion())
-		ps = PoseStamped()
-		ps.header.frame_id = self.robot.get_planning_frame()
-		self.scene.add_box('table', ps, (0.6, 0.75, 0.5))
-
-	# seems ROS doesn't allow determining the argument and the output types
-	# def pick(self, cube: Cube) -> bool: 
 	def pick(self, cube):
-		# decide on arm to use
 		while cube.pose == None: pass
-		use_left = True if cube.pose.position.y > 0 else False
-		move_group = self.move_group_left if use_left else self.move_group_right
+		# use_left = True if cube.pose.position.y > 0 else False # should be done somewhere else
 
 		# create pre-pick (10 cm above pick posistion) & pick position
-		pick_poses_list = self._get_pre_pickplace_poses(False, cube.pose)
-		used_pose = self._execute_pick(use_left, pick_poses_list)
-		
-		# retract via the pre-pick pose to the watch position
-		self._retract(use_left, used_pose)
+		pick_poses_list = self._get_pre_pick_poses(cube.pose)
+		self._execute_pick(pick_poses_list)
 
-		return use_left
+		return 0 # should correspond to success
 
-	def place(self, use_left, place_pose):
+	def place(self, place_pose):
 		# select arm to use
 		move_group = self.move_group_left if use_left else self.move_group_right
 
 		# create pre-place (10 cm above place posistion) & place position
-		place_poses_list = self._get_pre_pickplace_poses(False, place_pose)
-		used_pose = self._execute_place(use_left, place_poses_list)
+		place_poses_list = self._get_pre_place_poses(place_pose)
+		self._execute_place(place_poses_list)
 
-		# create post place position (same as pre-place position) and retract
-		self._retract(use_left, used_pose)
+		return 0 # should be success
 
-	def move_left_to_start_position(self):
-		self.move_group_left.go(self._arm_straight_pose, wait=True)
-		self.move_group_left.go(self._start_pose, wait=True)
-		self.move_group_left.stop() # to ensure there is not residual movement
+	def move_to_start_position(self):
+		self.move_group.go(self._arm_straight_pose, wait=True)
+		self.move_group.go(self._start_pose, wait=True)
+		self.move_group.stop() # to ensure there is not residual movement
 
-	def move_right_to_start_position(self):
-		self.move_group_right.go(self._arm_straight_pose, wait=True)
-		self.move_group_right.go(self._start_pose, wait=True)
-		self.move_group_right.stop() # to ensure there is not residual movement
+	def move_to_watch_position(self):
+		self.move_group.go(self._look_at_pose, wait=True)
+		self.move_group.stop() # to ensure there is not residual movement
 
-	def move_both_to_start_position(self):
-		self.move_left_to_start_position()
-		self.move_right_to_start_position()
-
-	def move_left_to_watch_position(self):
-		self.move_group_left.go(self._look_at_pose, wait=True)
-		self.move_group_left.stop() # to ensure there is not residual movement
-
-	def move_right_to_watch_position(self):
-		self.move_group_right.go(self._look_at_pose, wait=True)
-		self.move_group_right.stop() # to ensure there is not residual movement
-
-	def move_both_to_watch_position(self):
-		# later TODO: make simultaneuos
-		self.move_left_to_watch_position()
-		self.move_right_to_watch_position()
-
-	def close_gripper(self, use_left):
-		gripper_client, joint_names = (self._gripper_left_client, self._gripper_left_joint_names) if use_left else (self._gripper_right_client, self._gripper_right_joint_names)
-		
+	def set_gripper(self, goal_state):
 		# create goal for the action client for the gripper
 		goal = FollowJointTrajectoryGoal()
 
 		header = Header()
 		header.stamp = rospy.Time.now()
-		goal.trajectory = JointTrajectory(header, joint_names, self._gripper_closed)
+		goal.trajectory = JointTrajectory(header, self._gripper_joint_names, goal_state)
 		
 		goal.goal_time_tolerance = rospy.Duration.from_sec(0.5)
 		
-		gripper_client.send_goal_and_wait(goal)
+		self._gripper_client.send_goal_and_wait(goal)
 
-	def open_gripper(self, use_left):
-		gripper_client, joint_names = (self._gripper_left_client, self._gripper_left_joint_names) if use_left else (self._gripper_right_client, self._gripper_right_joint_names)
-		
-		# create goal for the action client for the gripper
-		goal = FollowJointTrajectoryGoal()
-
-		header = Header()
-		header.stamp = rospy.Time.now()
-		goal.trajectory = JointTrajectory(header, joint_names, self._gripper_opened)
-		
-		goal.goal_time_tolerance = rospy.Duration.from_sec(1.0)
-
-		gripper_client.send_goal_and_wait(goal)
-
-	def _execute_pick(self, use_left, pick_poses_list):
-		move_group = self.move_group_left if use_left else self.move_group_right
-
-		used_pose = None # TODO add error handling if stays none
+	def _execute_pick(self, pick_poses_list):
+		# TODO add error handling 
 		for pick_poses in pick_poses_list:
-			plan, fraction = move_group.compute_cartesian_path(
+			plan, fraction = self.move_group.compute_cartesian_path(
 				pick_poses,  # waypoints to follow
 				0.01,  # eef_step
 				0.0)  # jump_threshold
@@ -207,47 +132,66 @@ class Grasp:
 				used_pose = pick_poses[0]
 				break
 	
-		res = move_group.execute(plan, wait=True) # later TODO: Somehow enable that the other arm can be started while the first one is in movement
+		res = self.move_group.execute(plan, wait=True) # later TODO: Somehow enable that the other arm can be started while the first one is in movement
 		
-		self.close_gripper(use_left)
+		self.set_gripper(self._gripper_closed)
+
+		self._retract([pick_poses[0]])
 		return used_pose
 
-	def _execute_place(self, use_left, place_poses_list):
-		move_group = self.move_group_left if use_left else self.move_group_right
-
-		used_pose = None # TODO add error handling if stays none
+	def _execute_place(self, place_poses_list):
+		# TODO: add error handling
 		for place_poses in place_poses_list:
-			plan, fraction = move_group.compute_cartesian_path(place_poses, 0.01, 0.0)
+			plan, fraction = self.move_group.compute_cartesian_path(place_poses, 0.01, 0.0)
 			if fraction == 1: # could compute whole trajectory
-				used_pose = place_poses[0]
 				break
 
-		res = move_group.execute(plan, wait=True)
+		res = self.move_group.execute(plan, wait=True)
 		
-		self.open_gripper(use_left)
-		return used_pose
+		self.set_gripper(self._gripper_opened)
 
-	def _retract(self, use_left, retract_pose):
-		move_group = self.move_group_left if use_left else self.move_group_right
+		self._retract([place_poses[0]])
 
-		plan, _ = move_group.compute_cartesian_path(
-			[retract_pose], 0.01, 0.0)  
-		move_group.execute(plan, wait=True)
+	def _retract(self, retract_poses):
+		plan, _ = self.move_group.compute_cartesian_path(retract_poses, 0.01, 0.0)  
+		self.move_group.execute(plan, wait=True)
 
 		# move to watch position
-		self.move_left_to_watch_position() if use_left else self.move_right_to_watch_position()
+		self.move_to_watch_position()
 
-	def _get_pre_pickplace_poses(self, if_pick, cube_pose):
+	def _get_pre_pick_poses(self, cube_pose):
 		poses = []
-		
+
+		for rotate_z in self._rotate_z_90_degs:
+			target_pose = copy.deepcopy(cube_pose)
+			approach_pose = copy.deepcopy(cube_pose)
+
+			q = quat_to_array(target_pose.orientation)
+			# approach from left, right, front or back
+			q = tr.quaternion_multiply(q, rotate_z)
+			# get hom rot matrix from quaternion
+			R = tr.quaternion_matrix(q)
+			#  move target pose to -10cm in x direction in its own frame
+			(approach_pose.position.x, approach_pose.position.y, approach_pose.position.z) = (approach_pose.position.x, approach_pose.position.y, approach_pose.position.z) - 0.1 * R[:3, 0]
+
+			# have the end-effector approach from 10 cm above the cube
+			target_pose.orientation = approach_pose.orientation = array_to_quat(q)
+			pre_pose = copy.deepcopy(approach_pose)
+			pre_pose.position.z += 0.1
+
+			poses.append([pre_pose, approach_pose, target_pose])
+
+		return poses
+
+	def _get_pre_place_poses(self, cube_pose):
+		poses = []
+
 		for rotate_z in self._rotate_z_90_degs:	
 			target_pose = copy.deepcopy(cube_pose)
 			q = quat_to_array(target_pose.orientation)
-			q = tr.quaternion_multiply(q, rotate_z)
-		
-			# approach angle, in case wanted
-			if not if_pick:
-				q = tr.quaternion_multiply(q, self._rotate_y_45_deg)
+			q = tr.quaternion_multiply(q, rotate_z) # approach from left, right, front or back
+
+			q = tr.quaternion_multiply(q, self._rotate_y_45_deg) # approach angle to horizontal
 
 			target_pose.orientation = array_to_quat(q)
 			pre_pose = copy.deepcopy(target_pose)
